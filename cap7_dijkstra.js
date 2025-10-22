45cdd9a91cc47eef06f703b8f8cc1d49a44de057
Gabriel Gomes <gggabriel368@gmail.com>
2025-10-22 09:59:31 -0300

    implementação do dijkstra em andamento

--- cap7_dijkstra.js
+++ cap7_dijkstra.js
@@ -10,5 +10,42 @@
    4 - Calcule o caminho final
 */
 
-/* Esse algortimo só funciona em gráficos em que não existem ciclos/loopings entre vértices */
+/* Esse algortimo só funciona em gráficos em que não existem ciclos/loopings entre vértices e que não existem
+   números negativos */
 
+let grafo = {
+   "inicio": {
+      "a": 6,
+      "b": 2
+   },
+   "a": {
+      "fim": 1
+   },
+   "b": {
+      "a": 3,
+      "fim": 5
+   },
+   "fim": {}
+}
+
+let custo = {
+   "a": 6,
+   "b": 2,
+   "fim": Infinity
+}
+
+let pais = {
+   "a": "inicio",
+   "b": "inicio",
+   "fim": undefined
+}
+
+let processados = []
+
+function dijkstra(){
+   
+}
+
+console.log(grafo)
+console.log(custo)
+console.log(pais)
