91872691f22a4763959924bc362897f9ff3a290b
Gabriel Gomes <gggabriel368@gmail.com>
2026-05-08 16:29:27 -0300

    explain k-neighbour

--- cap10_k_vizinhos_mais_proximos.js
+++ cap10_k_vizinhos_mais_proximos.js
@@ -1,7 +1,14 @@
 /*
     O que é?
     É um algoritmo usado para tentar classificar ou agrupar um item usando 
-    seus vizinhos mais próximos e com peculiaridades em comum.
+    seus vizinhos mais próximos e com peculiaridades em comum utilizando calculos de
+    similaridade de cossenos ou distancia entre dois pontos.
+    Além de agrupar, ele também faz regressões, ou seja, tenta prever um valor 
+    numérico usando os vizinhos mais próximos, que foram obtidos por meio da distancia
+    entre dois pontos ou similaridade de cossenos.
+
+    *Dependendo da característica a ser comparada, os resultados podem ser 
+    diferentes.
 */
 
 
