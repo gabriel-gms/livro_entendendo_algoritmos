4a0ab59138274da16924c2f678f8054d504e9db9
Gabriel Gomes <gggabriel368@gmail.com>
2026-05-23 18:23:47 -0300

    inorder_traversal

--- cap11_arvore_binaria.js
+++ cap11_arvore_binaria.js
@@ -1,34 +1,74 @@
 /*
     Árvores binárias
+
+    Código fonte é referencia de https://github.com/python-cafe/data_structures/blob/master/arvores/examples.py
+    refatorado em javascript
 */
 
-function node(data){
-    data = data;
-    let left = null;
-    let right = null;
-    return { data, left, right };
-}
-
-function binaryTree(data=null, node=null){
-    if(node){
-        let root = node
-        return { root };
-    }
-    else if(data){
-        node = node(data);
-        let root = node;
-        return { root };
-    }
-    else {
-        let root = null;
-        return { root };
+class Node {
+    constructor(data){
+        this.data = data
+        this.left = null;
+        this.right = null;
     }
 }
 
-let tree = binaryTree();
-let tree.root.left = node(1);
-let tree.root.right = node(2);
+class BinaryTree {
+    constructor(data=null, node=null){
+        if(node){
+            this.root = node;
+        }
+        else if(data){
+            node = Node(data);
+            this.root = node;
+        }
+        else {
+            this.root = null;
+        }
+    }
 
-console.log(tree);
-console.log(tree.root.left);
-console.log(tree.root.right);
+    simetric_traversal(node=null){
+        if(node === null){
+            node = this.root
+        }
+        if(node.left){
+            process.stdout.write('( ');
+            this.simetric_traversal(node.left);
+        }
+        process.stdout.write(node.data+' ');
+        if(node.right){
+            this.simetric_traversal(node.right);
+            process.stdout.write(' ) ');
+        }
+    }
+}
+
+function inorder_example_tree(){
+
+    let tree = new BinaryTree();
+    let n1 = new Node('a');
+    let n2 = new Node('+');
+    let n3 = new Node('*');
+    let n4 = new Node('b');
+    let n5 = new Node('-');
+    let n6 = new Node('/');
+    let n7 = new Node('c');
+    let n8 = new Node('d');
+    let n9 = new Node('e');
+    
+    n6.left = n7
+    n6.right = n8
+    n5.left = n6
+    n5.right = n9
+    n3.left = n4
+    n3.right = n5
+    n2.left = n1
+    n2.right = n3
+    
+    tree.root = n2
+    tree.simetric_traversal();
+    return tree
+}
+
+
+inorder_example_tree();
